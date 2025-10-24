668b59e47006483e9da9d74e8050d775e8305074
LakshyaBagani <Lakshyabagani123@gmail.com>
2025-10-24 20:38:26 +0530

    Change deloped backend url

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173" , "http://localhost:5174"],
+  origin: ["*"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
 }));
